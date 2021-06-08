986715610c5334202c380c2305758602561bb9e2
amarok77 <caltaste@inwind.it>
2021-06-08 14:37:26 +0200

    Update addon_deps.sh

--- bticino-x8000/addon_deps.sh
+++ bticino-x8000/addon_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apk add python3 py3-pip #mosquitto-clients
+apk add python3 py3-pip mosquitto-clients
 pip3 install --upgrade pip wheel setuptools
-pip3 install --trusted-host pypi.python.org -r requirements-addon.txt
+#pip3 install --trusted-host pypi.python.org -r requirements-addon.txt
 # Remove packages we only needed for installation
 #apk del linux-headers libc-dev libffi-dev python3-dev gcc
